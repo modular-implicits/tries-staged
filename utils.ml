9cfc8925c8c0ada57e93e92ef8efa32de3150ef4
pxeger <_@pxeger.com>
2023-09-18 09:12:03 +0100

    use fold

--- utils.ml
+++ utils.ml
@@ -1,5 +1,4 @@
 let from_list {M: S.Mapping} xs =
-  let m = ref (M.empty ()) in
-  List.iter (fun (k, v) -> m := M.insert k v !m) xs; !m
+  List.fold_left (fun m (k, v) -> M.insert k v m) (M.empty ()) xs
 
 let entries {M: S.Mapping} m = M.entries m
